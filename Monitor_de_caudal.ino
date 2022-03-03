155b9dc0feaab144842afa870ec2c223eddd4b9d
GonzaloGassele <48393196+GonzaloGassele@users.noreply.github.com>
2022-03-03 12:10:32 -0300

    Add files via upload

--- Monitor_de_caudal.ino
+++ Monitor_de_caudal.ino
@@ -12,8 +12,10 @@ volatile int NumPulsos; //variable para la cantidad de pulsos recibidos
 int PinSensor = 2;    //Sensor conectado en el pin 2
 float factor_conversion=4.8; //para convertir de frecuencia a caudal
 int frecuencia; //obtenemos la Frecuencia de los pulsos en Hz
-float caudal_L_m;
-bool bandera;
+unsigned long tiempoAnterior1 = 0;
+unsigned long pulsos_Acumulados1 = 0;
+float litros1; // Litros acumulados Caudalímetro 1
+float litros_minuto1; // Caudal 1 (lts/Min)
 
 
 void flujo1()
@@ -21,44 +23,29 @@ void flujo1()
   NumPulsos++;  //incrementamos la variable de pulsos
 } 
 
-//---Función para obtener frecuencia de los pulsos--------
-int ObtenerFrecuencia() 
-{
-  NumPulsos = 0;   //Ponemos a 0 el número de pulsos
-  interrupts();    //Habilitamos las interrupciones
-  delay(1000);   //muestra de 1 segundo
-  noInterrupts(); //Desabilitamos las interrupciones
-  frecuencia=NumPulsos; //Hz(pulsos por segundo)
-  return frecuencia;
-}
 
 void setup()
 {
   Serial.begin(9600);//Velocidad del puerto serial de arduino
   MySerial.begin(9600);
-  pinMode(PinSensor,INPUT);
-  attachInterrupt(0,flujo1,RISING);
+  pinMode(PinSensor,INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(PinSensor),flujo1,RISING);
+  tiempoAnterior1 = millis(); // Guardo el tiempo que tarda el ejecutarse el setup
   Serial.println("Empezando");
-  delay(2000);//Tiempo prudencial para el escudo inicie sesión de red con tu operador
-  bandera= true;
-  Serial.println("paso el setup"); 
 } 
 
 
 
 void loop(){
-  Serial.println("Entro en el loop");
-  Caudal();
-  Serial.println("Paso el caudal");  
-  comandosAT();//Llama a la función comandosAT
-  Serial.println("Paso los comando At");  
+  Caudal();  
+  comandosAT();//Llama a la función comandosAT 
   mostrarDatosSeriales();
-  Serial.println("LLego al final del loop");
 }
 
 
 //Funcion de envio de datos
 void comandosAT(){
+  Serial.println("Entro al comando at");
   MySerial.println("AT+CIPSTATUS");//Consultar el estado actual de la conexión
   delay(2000);
   MySerial.println("AT+CIPMUX=0");//comando configura el dispositivo para una conexión IP única o múltiple 0=única
@@ -81,9 +68,9 @@ void comandosAT(){
   MySerial.println("AT+CIPSEND");//ENVÍA DATOS A TRAVÉS DE una CONEXIÓN TCP O UDP
   delay(4000);
   mostrarDatosSeriales();
-  Serial.print("el caudal es" );
-  Serial.println(caudal_L_m);
-  String datos="GET https://api.thingspeak.com/update?api_key=LJO8PPE93VVYDQAO&field1=0" + String(caudal_L_m);
+  Serial.print("el caudal es " );
+  Serial.println(litros_minuto1);
+  String datos="GET https://api.thingspeak.com/update?api_key=LJO8PPE93VVYDQAO&field1=0" + String(litros_minuto1);
   MySerial.println(datos);//Envía datos al servidor remoto
   delay(4000);
   mostrarDatosSeriales();
@@ -94,30 +81,36 @@ void comandosAT(){
   MySerial.println("AT+CIPSHUT");//Cierra la conexión(Desactiva el contexto GPRS PDP)
   delay(5000);
   mostrarDatosSeriales();
+  Serial.print("Salio del comando at");
 } 
 
 //Muestra los datos que va entregando el sim900
 
 void mostrarDatosSeriales()
 {
-delay(500);
-while (Serial.available())
-{
-MySerial.write(Serial.read());//Forward what MySerial received to Software MySerial Port
-}
-while (MySerial.available())
-{
-Serial.write(MySerial.read());//Forward what MySerial received to Software MySerial Port
-}
+  delay(500);
+  while (Serial.available())
+  {
+  MySerial.write(Serial.read());//Forward what MySerial received to Software MySerial Port
+  }
+  while (MySerial.available())
+  {
+  Serial.write(MySerial.read());//Forward what MySerial received to Software MySerial Port
+  }
 }
 
 // Calcula el caudal
 
 void Caudal()
 {
-        frecuencia= ObtenerFrecuencia(); 
-        caudal_L_m=frecuencia/factor_conversion; //calculamos el caudal en L/m
-  
+  if(millis() - tiempoAnterior1 > 1000)
+    {
+      tiempoAnterior1 = millis(); // Actualizo el nuevo tiempo
+      pulsos_Acumulados1 += NumPulsos; // Número de pulsos acumulados
+      litros_minuto1 = (NumPulsos /factor_conversion); // Q = frecuencia / 4.8 (L/Min)
+      litros1 = pulsos_Acumulados1*1.0/300; // Cada 300 pulsos son un litro
+      NumPulsos = 0; // Pongo nuevamente el número de pulsos a cero 
+    }
 }
 
 //Apaga el sim y arduino por 20 minutos
